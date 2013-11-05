bc9d0bf91963e98d2dcea27dc5ec19cae2959394
Nick Chris <nicholas.chris@ttiltd.com>
2013-11-05 12:28:24 -0700

    Blackjack with Bonus

--- blackjack.rb
+++ blackjack.rb
@@ -1,101 +1,144 @@
-suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
-cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
-
-deck = cards.product(suits)
-deck.shuffle!
-
-#Starting Cards
-player_hand = []
-dealer_hand = []
-
-player_total = 0
-dealer_total = 0
-
-2.times do
-	player_hand << deck.pop
-end
-
-2.times do
-	dealer_hand << deck.pop
-end
-
-#Totalling cards
-
+# Totalling card values
 def calc_total(cards) #cards = player's or dealer's array of cards
-	hand = cards.map{ |x| x[0] }
-	total = 0
+  hand = cards.map{ |x| x[0] }
+  total = 0
 
-	hand.each do |val|
-		if val == 'Ace'
-			if total + 11 > 21
-				total += 1
-			else 
-				total += 11
-			end
-		elsif val.to_i == 0
-			total += 10
-		else
-			total += val.to_i
-		end
-	end
+  hand.each do |val|
+    if val == 'Ace'           
+      total += 11
+    elsif val.to_i == 0     
+      total += 10
+    else                    
+      total += val.to_i
+    end
+  end
 
-	total
+  hand.select{|e| e == 'Ace'}.count.times do
+    total -= 10 if total > 21 
+  end
+
+  total
 end
 
+def say(msg)
+  puts "==> #{msg}"
+end
 
-#Output
-puts "----- Dealer hand -----"
-puts dealer_hand[0][0] + " of " + dealer_hand[0][1]
-puts dealer_hand[1][0] + " of " + dealer_hand[1][1]
+puts "~*~*~*~*~*~ Hi and welcome to Blackjack ~*~*~*~*~*~"
+say "What is your name?"
+player_name = gets.chomp.downcase.capitalize
 
-puts "==> Dealer's total is #{calc_total(dealer_hand)}" 
-puts
+# Loop to continue game
+play_again = true
+while play_again == true
+  suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+  cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
 
-puts "----- Player hand -----"
-puts player_hand[0][0] + " of " + player_hand[0][1]
-puts player_hand[1][0] + " of " + player_hand[1][1]
-	
-puts "==> Your total is #{calc_total(player_hand)} "
-puts
+  # Use 3 decks
+  3.times do 
+    deck << cards.product(suits)
+  end
+  deck.flatten!(1)
+  deck.shuffle!
 
-#hit or stay loop
-card_counter = 1
-while calc_total(player_hand) <= 21
-	puts "==> Would you like to 1) Hit or 2) Stay?"
-  hit_choice = gets.chomp
-  if hit_choice == '1'
-  	player_hand << deck.pop
-  	card_counter += 1
-  	puts player_hand[card_counter][0] + " of " + player_hand[card_counter][1]
-  	puts "==> Your total is #{calc_total(player_hand)} "
-  	if calc_total(player_hand) > 21
-  		puts "==> Sorry you bust!"
-  		break
-  	end
-  elsif hit_choice == '2'
-  	puts "==> You stand with #{calc_total(player_hand)}"
-  	card_counter = 1
-  	while calc_total(dealer_hand) < 17
-  		puts "==> Dealer has #{calc_total(dealer_hand)} and must hit..."
-  		dealer_hand << deck.pop
-  		card_counter += 1
-  		puts dealer_hand[card_counter][0] + " of " + dealer_hand[card_counter][1]
-  		puts "==> Dealer now has #{calc_total(dealer_hand)}"
-  	end
-  	puts
-		if calc_total(player_hand) > calc_total(dealer_hand)
-			puts "==> You have #{calc_total(player_hand)} and Dealer has #{calc_total(dealer_hand)}"
-			puts "==> You Win!!!"
-		elsif calc_total(player_hand) == calc_total(dealer_hand)
-			puts "==> #{calc_total(player_hand)} to #{calc_total(dealer_hand)}"
-			puts "==> It is a push."
-		else
-			puts "==> Sorry, Dealer wins with #{calc_total(dealer_hand)}"
-		end
-  	break
-  else
-  	puts "==> Please enter 1 for 'hit or 2 for 'stay'"
+  #Starting Cards
+  player_hand = []
+  dealer_hand = []
+  player_total = calc_total(player_hand)
+  dealer_total = calc_total(dealer_hand)
+
+  2.times{player_hand << deck.pop}
+  2.times{dealer_hand << deck.pop}
+
+  #Output initial deal
+  puts "------ Dealer's hand ------"
+  puts dealer_hand[0][0] + " of " + dealer_hand[0][1]
+  puts dealer_hand[1][0] + " of " + dealer_hand[1][1]
+
+  say "Dealer's total is #{calc_total(dealer_hand)}" 
+  puts
+
+  puts "------ #{player_name}'s hand ------"
+  puts player_hand[0][0] + " of " + player_hand[0][1]
+  puts player_hand[1][0] + " of " + player_hand[1][1]
+    
+  say "#{player_name}'s total is #{calc_total(player_hand)} "
+  puts
+
+  # Player hit/stay loop
+  if calc_total(player_hand) == 21
+    say "Winner! Winner!! You have blackjack!"
+    exit
+  end
+
+  while calc_total(player_hand) < 21
+    say "Would you like to 1) Hit or 2) Stay?"
+    hit_choice = gets.chomp
+    if !['1', '2'].include?(hit_choice)
+      puts "Sorry, you must enter 1 or 2"
+      next
+    elsif hit_choice == '1'
+      player_hand << deck.pop
+      puts player_hand.last[0] + " of " + player_hand.last[1]
+      puts
+      say "#{player_name}'s total is #{calc_total(player_hand)}."
+        if calc_total(player_hand) == 21
+          say "You have 21, and is the dealer's turn."
+          break
+        elsif calc_total(player_hand) > 21
+          say "Sorry #{player_name}, you have busted. Better luck next time."
+          puts
+          break
+        end            
+    elsif hit_choice == '2'
+      say "#{player_name} stands with #{calc_total(player_hand)}."
+      puts
+      break
+    end
+  end
+
+  # Dealer hit/stay loop
+  if calc_total(dealer_hand) == 21
+    say "Sorry #{player_name}, dealer hit blackjack. You lose."
+    #break
+  end
+
+
+  while calc_total(dealer_hand) < 17
+    say "Dealer has #{calc_total(dealer_hand)} and must hit..."
+    dealer_hand << deck.pop
+    puts dealer_hand.last[0] + " of " + dealer_hand.last[1] 
+    say "Dealer now has #{calc_total(dealer_hand)}."
+    puts
+    if calc_total(dealer_hand) > 21
+      say "Dealer busts with #{calc_total(dealer_hand)}! #{player_name} Wins!!!"
+      puts
+      break
+    end
+  end
+
+  if calc_total(player_hand) <= 21 && calc_total(dealer_hand) <= 21
+    if (calc_total(player_hand) > calc_total(dealer_hand))
+      say "#{player_name} wins with #{calc_total(player_hand)}!!"
+    elsif calc_total(player_hand) == calc_total(dealer_hand)
+      say "#{calc_total(player_hand)} to #{calc_total(dealer_hand)}"
+      say "Game is a push."
+    else
+      say "Dealer wins with #{calc_total(dealer_hand)}"
+    end
+  end
+
+  say "Would you like to play again?(Y/N)"
+  choice = gets.chomp.downcase
+
+  if !['y', 'n'].include?(choice)
+    say "Sorry, enter in Y or N"
+  elsif choice == 'y'
+    puts
+    puts
+    play_again = true
+  else choice = 'n'
+    say "Thanks for playing #{player_name}"
+    play_again = false
   end
 end
-
-
